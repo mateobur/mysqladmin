ab8f3f364430098f564442bf3e8e4ed22685d85e
Mateo Burillo <mburillo@zentyal.com>
2013-01-23 00:07:30 +0100

    First version that actually shows up in the interface

--- src/EBox/MyAdmin.pm
+++ src/EBox/MyAdmin.pm
@@ -81,110 +81,9 @@ sub _setConf
     my ($self) = @_;
 
     my $params;
-    if ($self->_usesEBoxMail()) {
-        $params = $self->_confFromMail();
-    } else {
-        $params = $self->_confForRemoteServer();
-    }
-
-    my $managesieve = $self->_managesieveEnabled();
 
     my $options = $self->model('Options');
-    push @{ $params },
-        (
-         managesieve => $managesieve,
-         productName => $options->productName,
-         desKey      => $self->desKey(),
-        );
-
-    $self->writeConfFile(
-                         MAIN_INC_FILE,
-                         'webmail/main.inc.php.mas',
-                         $params,
-                        );
-
-    if ($managesieve) {
-        $self->_setManageSievePluginConf();
-    }
-
-    $self->_setWebServerConf();
 }
-
-sub _managesieveEnabled
-{
-    my ($self) = @_;
-    if ($self->_usesEBoxMail()) {
-        my $mail = EBox::Global->modInstance('mail');
-        return $mail->managesieve();
-    } else {
-        my $remoteConfRow = $self->model('RemoteServerConfiguration')->row();
-        return $remoteConfRow->elementByName('managesieve')->value();
-    }
-}
-
-sub _setManageSievePluginConf
-{
-    my ($self) = @_;
-
-    my $params;
-    if ($self->_usesEBoxMail()) {
-        $params = [
-                   host => 'localhost',
-                   port => 4190,
-                   tls  => 0,
-                  ]
-    } else {
-        $params =
-            $self->model('RemoteServerConfiguration')->getSieveConfiguration();
-    }
-
-
-    $self->writeConfFile(
-                         SIEVE_PLUGIN_INC_ETC_FILE,
-                         'webmail/managesieve-config.php.inc.mas',
-                         $params
-                        );
-    # removing /usr file  and creatign a link to avoid package upgrades troubles
-    EBox::Sudo::root('rm -f ' . SIEVE_PLUGIN_INC_USR_FILE);
-    EBox::Sudo::root('ln -s ' . SIEVE_PLUGIN_INC_ETC_FILE . ' ' .
-                                SIEVE_PLUGIN_INC_USR_FILE);
-
-}
-
-sub _confFromMail
-{
-    my ($self) = @_;
-    my $mail = EBox::Global->modInstance('mail');
-    my @conf;
-
-    if ($mail->imap()) {
-        @conf = (
-                 imapServer => '127.0.0.1',
-                 imapPort   => 143,
-                );
-    } elsif ($mail->imaps()) {
-        @conf = (
-                 imapServer => 'ssl://127.0.0.1',
-                 imapPort => 993,
-                );
-    } elsif ($self->isEnabled) {
-            throw EBox::Exceptions::External(__('Neither IMAP nor IMAPS service enabled.'));
-    }
-
-    push @conf, (
-                 smtpServer => '127.0.0.1',
-                 smtpPort   => 25,
-                );
-
-    return \@conf;
-}
-
-sub _confForRemoteServer
-{
-    my ($self) = @_;
-    return $self->model('RemoteServerConfiguration')->getConfiguration();
-}
-
 
 # Group: Public methods
 
@@ -202,7 +101,7 @@ sub menu
 
     $root->add(
                new EBox::Menu::Item(
-                   'url' => 'WebMail/View/Options',
+                   'url' => 'MyAdmin/View/Options',
                    'text' => $self->printableName(),
                    'separator' => 'Infrastructure',
                    'order' => 455,
@@ -219,82 +118,6 @@ sub menu
 #
 #        <EBox::Module::Service::usedFiles>
 #
-sub usedFiles
-{
-    my ($self) = @_;
-
-    my $files = [
-        {
-            'file' => MAIN_INC_FILE,
-            'reason' => __('To configure Roundcube webmail.'),
-            'module' => 'webmail'
-        },
-        {
-            'file' => SIEVE_PLUGIN_INC_USR_FILE,
-            'reason' => __('To configure managesieve Roundcube webmail plugin.'),
-            'module' => 'webmail'
-        },
-    ];
-
-    my $destFile = EBox::WebServer::GLOBAL_CONF_DIR . 'ebox-webmail';
-    push(@{$files}, { 'file' => $destFile, 'module' => 'webmail',
-                      'reason' => __('To configure the webmail on the webserver.') });
-    return $files;
-}
-
-# Method: actions
-#
-#        Explain the actions the module must make to configure the
-#        system. Check overriden method for details
-#
-# Overrides:
-#
-#        <EBox::Module::Service::actions>
-sub actions
-{
-    return [
-            {
-             'action' => __('Create MySQL Roundcube webmail database.'),
-             'reason' => __('This database will store the data needed by Roundcube.'),
-             'module' => 'webmail'
-            },
-            {
-             'action' => __('Add webmail link to www data directory.'),
-             'reason' => __('Webmail will be accesible at http://ip/webmail/.'),
-             'module' => 'webmail'
-            },
-
-           ];
-}
-
-# Method: enableActions
-#
-#        Run those actions explain by <actions> to enable the module
-#
-# Overrides:
-#
-#        <EBox::Module::Service::enableActions>
-#
-sub enableActions
-{
-    my ($self) = @_;
-
-    if ($self->_usesEBoxMail()) {
-        my $mail = EBox::Global->modInstance('mail');
-        if ((not $mail->imap())and (not $mail->imaps()) ) {
-            throw EBox::Exceptions::External(__x('Webmail module needs IMAP or IMAPS service enabled if ' .
-                                                 'using Zentyal mail service. You can enable it at ' .
-                                                 '{openurl}Mail -> General{closeurl}.',
-                                                 openurl => q{<a href='/Mail/Composite/General'>},
-                                                 closeurl => q{</a>}));
-        }
-    }
-
-    # Execute enable-module script
-    $self->SUPER::enableActions();
-
-    $self->_generateDesKeyFile();
-}
 
 # Method: disableActions
 #
@@ -316,62 +139,7 @@ sub disableActions
 #
 sub enableModDepends
 {
-    my ($self) = @_;
-    if ($self->_usesEBoxMail()) {
-        return ['mail', 'webserver'];
-    }
-
     return ['webserver'];
-}
-
-sub _usesEBoxMail
-{
-    my ($self) = @_;
-
-    return $self->model('OperationMode')->usesEBoxMail();
-}
-
-sub validateIMAPChanges
-{
-    my ($self, $imap, $imaps) = @_;
-    if (not $self->_usesEBoxMail()) {
-        return;
-    }
-
-    if ($imap or $imaps) {
-        $self->setAsChanged();
-    } else {
-        throw EBox::Exceptions::External(__('You cannot disable both IMAP and IMAPS service because they are used by Webmail module.'));
-    }
-}
-
-sub _generateDesKeyFile
-{
-    my $desKey = '';
-    my $keyLength = 24;
-    # length of 24 chars
-    my @chars = ('a' .. 'z', 'A' .. 'Z', 0 .. 9,
-                 '(', ')', ',', '#',
-                 qw([ ] . : " ! @ $ % ^ & * < > ~ + /)
-                );
-    my $sizeChars = scalar @chars;
-
-    for (1 .. $keyLength) {
-        my $i = int(rand($sizeChars));
-        $desKey .= $chars[$i];
-    }
-
-    EBox::Sudo::root('rm -f ' . DES_KEY_FILE);
-
-    EBox::Sudo::command('touch ' . DES_KEY_FILE);
-    EBox::Sudo::command('chmod og-rwx ' . DES_KEY_FILE);
-    File::Slurp::write_file(DES_KEY_FILE, $desKey);
-}
-
-sub desKey
-{
-    my $desKey = File::Slurp::read_file(DES_KEY_FILE);
-    return $desKey;
 }
 
 sub _setWebServerConf
