832ea7c47622cc701467e2f6c30fccd8afcb040f
Mateo Burillo <mburillo@zentyal.com>
2013-01-24 00:21:21 +0100

    renames

--- src/EBox/MyAdmin.pm
+++ src/EBox/MyAdmin.pm
@@ -61,7 +61,7 @@ use constant {
 sub _create
 {
     my $class = shift;
-    my $self = $class->SUPER::_create(name => 'phpmyadmin',
+    my $self = $class->SUPER::_create(name => 'MyAdmin',
                                       printableName => __('MySQL Manager'),
                                       @_);
     bless($self, $class);
